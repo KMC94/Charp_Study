d3a4dc08598648a5c9b907ebb9629eaec3b3c477
kmc94 <money94611@naver.com>
2021-07-21 21:27:08 +0900

    Chap 03 완료(람다식과 LINQ 기초)

---
+++ CSharpStudy/Exercise3_1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+          
+            Exercise1_1(numbers);
+
+            Console.WriteLine("-----------");
+            Exercise1_2(numbers);
+
+            Console.WriteLine("-----------");
+            Exercise1_3(numbers);
+
+            Console.WriteLine("-----------");
+            Exercise1_4(numbers);
+        }
+
+        static void Exercise1_1(List<int> numbers)
+        {
+            var list = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+            Console.WriteLine(list);
+        }
+
+        static void Exercise1_2(List<int> numbers)
+        {
+            numbers.ForEach(n => Console.WriteLine(n / 2.0));
+        }
+
+        static void Exercise1_3(List<int> numbers)
+        {
+            var list = numbers.Where(n => n >= 50);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Exercise1_4(List<int> numbers)
+        {
+            var list = numbers.Select(n => n * 2).ToList();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ CSharpStudy/Exercise3_2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>
+            {
+                "Seoul", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            //Exercise2_1(names);
+            //Console.WriteLine("-----------");
+
+            //Exercise2_2(names);
+            //Console.WriteLine("-----------");
+
+            //Exercise2_3(names);
+            //Console.WriteLine("-----------");
+
+            Exercise2_4(names);
+            Console.WriteLine("-----------");
+        }
+
+        static void Exercise2_1(List<string> names)
+        {
+            var line = Console.ReadLine();
+
+            Console.WriteLine(names.FindIndex(s => s == line));
+        }
+
+        static void Exercise2_2(List<string> names)
+        {
+            Console.WriteLine(names.Count(s => s.Contains('o')));
+        }
+
+        static void Exercise2_3(List<string> names)
+        {
+            var list = names.Where(s => s.Contains('o'));
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Exercise2_4(List<string> names)
+        {
+            var list = names.Where(s => s.StartsWith("B"))
+                            .Select(s => s.Length);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
