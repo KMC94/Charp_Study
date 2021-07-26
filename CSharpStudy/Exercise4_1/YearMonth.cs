331d7d37ceca009f7830f5b2d25a585eb4be32dd
kmc94 <money94611@naver.com>
2021-07-26 22:24:52 +0900

    Chap 04 완료(기본 관용구 연습)

---
+++ CSharpStudy/Exercise4_1/YearMonth.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4_1
+{
+    public class YearMonth
+    {
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        public bool Is21Century
+        {
+            get
+            {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+        public YearMonth(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
+        static void Main(string[] args)
+        {
+            YearMonth[] yearMonths = new YearMonth[]
+            {
+                new YearMonth(1994, 6),
+                new YearMonth(1992, 4),
+                new YearMonth(1998, 3),
+                new YearMonth(1965, 10),
+                new YearMonth(1966, 11),
+            };
+            foreach (var item in yearMonths)
+            {
+                Console.WriteLine($"{item.Year}년 {item.Month}월");
+            }
+        }
+
+        public YearMonth AddOneMonth()
+        {
+            if(Month == 12)
+            {
+                return new YearMonth(this.Year + 1, 1);
+            }
+            else
+            {
+                return new YearMonth(this.Year + 1, this.Month + 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Year}년 {Month}월";
+        }
+    }
+}
---
+++ CSharpStudy/Exercise4_2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise4_1;
+
+namespace Exercise4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            YearMonth[] yearMonths = new YearMonth[]
+            {
+                new YearMonth(1994, 6),
+                new YearMonth(1992, 4),
+                new YearMonth(1998, 3),
+                new YearMonth(1965, 10),
+                new YearMonth(1966, 11),
+            };
+            foreach (var item in yearMonths)
+            {
+                Console.WriteLine($"{item.Year}년 {item.Month}월");
+            }
+
+            Exercise4_2(yearMonths);
+            Console.WriteLine("---------------");
+
+            Exercise4_4(yearMonths);
+            Console.WriteLine("---------------");
+
+            Exercise4_5(yearMonths);
+            Console.WriteLine("---------------");
+        }
+
+        static YearMonth Exercise4_2(YearMonth[] yearMonths)
+        {
+            foreach (var item in yearMonths)
+            {
+                if(item.Is21Century)
+                    return item;
+            }
+            return null;
+        }
+
+        static void Exercise4_4(YearMonth[] yearMonths)
+        {
+            YearMonth yearMonth = Exercise4_2(yearMonths);
+
+            if(yearMonth == null)
+                Console.WriteLine("21세기 데이터는 없습니다.");
+            else
+                Console.WriteLine($"{yearMonth.Year}년도");
+        }
+
+        static void Exercise4_5(YearMonth[] yearMonths)
+        {
+            YearMonth[] addOneMonths = yearMonths.Select(ym => ym.AddOneMonth()).ToArray();
+
+            foreach (var item in addOneMonths)
+            {
+                Console.WriteLine($"{item.Year}년 {item.Month}월");
+            }
+        }
+    }
+}
