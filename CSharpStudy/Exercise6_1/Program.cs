5fc8c35393aa071070e587b3979b1f95d2bbe75a
kmc94 <money94611@naver.com>
2021-08-06 21:55:14 +0900

    Chap 06 완료 배열과 List<T>를 LINQ로 가공하는 연습

--- CSharpStudy/Exercise6_1/Program.cs
+++ CSharpStudy/Exercise6_1/Program.cs
@@ -11,7 +11,56 @@ namespace Exercise6_1
     {
         static void Main(string[] args)
         {
-            var numbers = Enumerable.Repeat(-1, 20).ToList();
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            Exercise1_1(numbers);
+            Console.WriteLine("---------");
+            Exercise1_2(numbers);
+            Console.WriteLine("---------");
+            Exercise1_3(numbers);
+            Console.WriteLine("---------");
+            Exercise1_4(numbers);
+            Console.WriteLine("---------");
+            Exercise1_5(numbers);
+            Console.WriteLine("---------");
+        }
+
+        static void Exercise1_1(int[] numbers)
+        {
+            var max = numbers.Max();
+            Console.WriteLine(max);
+        }
+
+        static void Exercise1_2(int[] numbers)
+        {
+            var skip = numbers.Length - 2;
+            foreach (var item in numbers.Skip(skip))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Exercise1_3(int[] numbers)
+        {
+            var strs = numbers.Select(s => s.ToString());
+            foreach (var item in strs)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Exercise1_4(int[] numbers)
+        {
+            foreach (var item in numbers.OrderBy(n => n).Take(3))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Exercise1_5(int[] numbers)
+        {
+            var count = numbers.Distinct().Count(n => n > 10);
+            Console.WriteLine(count);
         }
     }
 }
---
+++ CSharpStudy/Exercise6_2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new List<Book> {
+               new Book { Title = "C# 프로그래밍의 상식", Price = 38000, Pages = 378 },
+               new Book { Title = "람다식과 LINQ의 비밀", Price = 25000, Pages = 312 },
+               new Book { Title = "원더풀 C# 라이프", Price = 29000, Pages = 385 },
+               new Book { Title = "독학 병렬처리 프로그래밍", Price = 48000, Pages = 464 },
+               new Book { Title = "구문으로 배우는 C# 입문", Price = 53000, Pages = 604 },
+               new Book { Title = "나도 할 수 있는 ASP.NET MVC", Price = 32000, Pages = 453 },
+               new Book { Title = "재미있는 C# 프로그래밍 교실", Price = 25400, Pages = 348 },
+            };
+
+            Exercise2_1(books);
+            Console.WriteLine("---------");
+            Exercise2_2(books);
+            Console.WriteLine("---------");
+            Exercise2_3(books);
+            Console.WriteLine("---------");
+            Exercise2_4(books);
+            Console.WriteLine("---------");
+            Exercise2_5(books);
+            Console.WriteLine("---------");
+            Exercise2_6(books);
+            Console.WriteLine("---------");
+            Exercise2_7(books);
+            Console.WriteLine("---------");
+        }
+
+        static void Exercise2_1(List<Book> books)
+        {
+            var book = books.FirstOrDefault(b => b.Title == "원더풀 C# 라이프");
+            Console.WriteLine("가격 : {0}, 페이지 수 : {1}", book.Price, book.Pages);
+        }
+
+        static void Exercise2_2(List<Book> books)
+        {
+            var count = books.Count(b => b.Title.Contains("C#"));
+            Console.WriteLine(count);
+        }
+
+        static void Exercise2_3(List<Book> books)
+        {
+            var averagePage = books.Where(b => b.Title.Contains("C#")).Average(b => b.Pages);
+            Console.WriteLine(averagePage);
+        }
+
+        static void Exercise2_4(List<Book> books)
+        {
+            var book = books.FirstOrDefault(b => b.Price >= 40000);
+            if(book != null)
+                Console.WriteLine(book.Title);
+        }
+
+        static void Exercise2_5(List<Book> books)
+        {
+            var maxPage = books.Where(b => b.Price < 40000).Max(b => b.Pages);
+            Console.WriteLine(maxPage);
+        }
+
+        static void Exercise2_6(List<Book> books)
+        {
+            var seleted = books.Where(b => b.Pages >= 400)
+                .OrderByDescending(b => b.Price);
+
+            foreach (var item in seleted)
+            {
+                Console.WriteLine("제목:{0} 가격{1}", item.Title, item.Price);
+            }
+        }
+
+        static void Exercise2_7(List<Book> books)
+        {
+            var seleted = books.Where(b => b.Title.Contains("C#") && b.Pages <= 500);
+            foreach (var item in seleted)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public int Pages { get; set; }
+    }
+}
